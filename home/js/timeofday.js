a94c42151b311ffe99086a28c7bc87d226363aae
Tiffany <32607019+tiffellio@users.noreply.github.com>
2021-09-02 15:16:34 -0700

    display time of day
    Greets you based on time of day on dashboard.

--- home/js/timeofday.js
+++ home/js/timeofday.js
@@ -1,18 +1,62 @@
 
-/*get time of day
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes();*/
+window.onload = function setDayTime(){
 
-var hourObj = new Date();
-var hour = hourObj.getHours();
+  // get current time and make a time object to compare with
+  var hourObj = new Date();
+  var hour = hourObj.getHours();
 
-alert(hour);
+  var minObj = new Date();
+  var minute = minObj.getMinutes();
 
-//if 12am to 11:59AM good morning name // 00 - 11:59
-  // get id=daytime from index.html
+  //alert(hour + " " + minute);
+
+  var currentTimeObj = new Date();
+  currentTimeObj.setHours(hour, minute);
+
+  // morning 12am to 11:59am
+  var startMorningTimeObj = new Date();
+  startMorningTimeObj.setHours(0, 0);
+
+  var endMorningTimeObj = new Date();
+  endMorningTimeObj.setHours(11, 59);
+
+  // afternoon 12pm to 5:59pm
+  var startAfternoonTimeObj = new Date();
+  startAfternoonTimeObj.setHours(12, 0);
+
+  var endAfternoonTimeObj = new Date();
+  endAfternoonTimeObj.setHours(17, 59);
+
+  //evening 6pm to 11:59pm
+  var startEveningTimeObj = new Date();
+  startEveningTimeObj.setHours(18, 0);
+
+  var endEveningTimeObj = new Date();
+  endEveningTimeObj.setHours(23, 59);
 
 
-// if 12pm to 5:59pm good afternoon name // 12 - 17:59
+  if ((currentTimeObj >= startMorningTimeObj) && (currentTimeObj <= endMorningTimeObj)) {
 
+    document.getElementById("daytime").innerHTML = "Good morning, ";
 
-//if 6pm to 11:59PM // 18 - 23:59
+  }
+
+  else if ((currentTimeObj >= startAfternoonTimeObj ) && (currentTimeObj <= endAfternoonTimeObj)) {
+
+    document.getElementById("daytime").innerHTML = "Good afternoon, ";
+
+  }
+
+  else if ((currentTimeObj >= startEveningTimeObj ) && (currentTimeObj <= endEveningTimeObj)) {
+
+    document.getElementById("daytime").innerHTML = "Good evening, ";
+
+  }
+
+  else {
+
+    alert("error in parsing time");
+
+  }
+
+}
