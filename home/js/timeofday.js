ab3d67595fe2abc62dae8d21f2fda5f2818e9b30
Tiffany <32607019+tiffellio@users.noreply.github.com>
2021-09-02 13:29:44 -0700

    get current hour

---
+++ home/js/timeofday.js
@@ -0,0 +1,18 @@
+
+/*get time of day
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes();*/
+
+var hourObj = new Date();
+var hour = hourObj.getHours();
+
+alert(hour);
+
+//if 12am to 11:59AM good morning name // 00 - 11:59
+  // get id=daytime from index.html
+
+
+// if 12pm to 5:59pm good afternoon name // 12 - 17:59
+
+
+//if 6pm to 11:59PM // 18 - 23:59
